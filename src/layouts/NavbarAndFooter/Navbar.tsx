7b97b6dc937ab7ad8536514ed92dfb78dd88782e
yutian <yzhang3674@gatech.edu>
2024-06-20 18:52:21 -0400

    add react router

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,21 +1,23 @@
+import { NavLink } from "react-router-dom";
+
 export const Navbar = () => {
-    return (
+  return (
     <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
       <div className='container-fluid'>
         <span className='navbar-brand'>Lv Cha</span>
-        <button className='navbar-toggler' type='button' 
+        <button className='navbar-toggler' type='button'
           data-bs-toggle='collapse' data-bs-target="#navbarNavDropdown"
           aria-controls='navbarNavDropdown' aria-expanded='false'
           aria-label='Toggle Navigation'>
-            <span className='navbar-toggler-icon'></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
         <div className='collapse navbar-collapse' id='navbarNavDropdown'>
           <ul className='navbar-nav'>
             <li className='nav-item'>
-              <a className="nav-link" href="#">Home</a>
+              <NavLink className="nav-link" to="/home">Home</NavLink>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href="#">Search Books</a>
+              <NavLink className='nav-link' to="/search">Search Books</NavLink>
             </li>
           </ul>
           <ul className='navbar-nav ms-auto'>
@@ -26,5 +28,5 @@ export const Navbar = () => {
         </div>
       </div>
     </nav>
-    );
+  );
 }
