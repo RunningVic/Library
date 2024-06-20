d486b1f14f4af37f9077edc3221b6dca1521a5c8
yutian <yzhang3674@gatech.edu>
2024-06-20 18:08:59 -0400

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
-import { SearchBooksPage } from './layouts/HomePage/SearchBooksPage/SearchBooksPage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
